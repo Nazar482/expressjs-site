093d6b3f86a21f4fc895dc6e841112c290b5f86f
Nazar <atamanyuk.nazar2@gmail.com>
2016-11-06 12:59:06 +0200

    make it better

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.get('*', routes.notFound);
 
 //Server
 app.set('port', process.env.PORT || 3000) ;
-var server = app.listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + server.address().port);
-});
+// var server = app.listen(app.get('port'), function(){
+//     console.log('Express server listening on port ' + server.address().port);
+// });
 
 
